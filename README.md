4245a0be03bb08312562c29edaa8d49f3862f853
Arne Peirs <arnepeirs@hotmail.com>
2017-04-12 23:49:11 +0200

    Add source

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # SwitchLanguage
+A plugin to switch the language in Kerbal Space Program to other languages.
+
+## How to help
+To contribute to this mod, please read the contributing guidelines.
+
+### Modifying the code
+
+1. Install a compiler/IDE
+    * For Windows I recommend [Visual Studio Community](https://www.visualstudio.com/vs/community/).
+    * For Linux I recommend [CodeLite](https://codelite.org/).
+    * For Mac I recommend [Xcode](https://developer.apple.com/xcode/).
+2. Fork and clone the repo.
+3. Open the project in your compiler/IDE.
+4. Modify the code.
+5. Compile the project.
+
+### Submitting pull requests
+
+1. Create a new branch, please don't work in your `master` branch directly.
+2. Push the branch to your fork and submit a pull request.
+3. Wait for evaluation by project members.
+
+## Credits
+* Olympic1 - Code
+
+## Licensing
+Copyright © 2017, Arne Peirs. All rights reserved.
+
+Written by Arne Peirs (Olympic1) for use in Kerbal Space Program.  
+This project may not be used in any derivative work without permission.
+
+https://github.com/KSP-Localization/SwitchLanguage
---
+++ Source/SwitchLanguage.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Switch Language
+ * Copyright © 2017, Arne Peirs (Olympic1)
+ * 
+ * Kerbal Space Program is Copyright © 2011-2017 Squad. See http://kerbalspaceprogram.com/.
+ * This project is in no way associated with nor endorsed by Squad.
+ * 
+ * Licensed under the terms of the MIT License.
+ * See https://opensource.org/licenses/MIT for full details.
+ */
+
+using System.IO;
+using KSP.Localization;
+using UnityEngine;
+
+namespace SwitchLanguage
+{
+    //[KSPAddon(KSPAddon.Startup.PSystemSpawn, true)]
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class SwitchLanguage : MonoBehaviour
+    {
+        private static SwitchLanguage instance;
+        private static readonly string filePath = KSPUtil.ApplicationRootPath + "/GameData/SwitchLanguage/settings.cfg";
+        //private static readonly string cachePath = KSPUtil.ApplicationRootPath + "/GameData/ModuleManager.ConfigCache";
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+            Init();
+        }
+
+        private static SwitchLanguage Init()
+        {
+            if (instance != null)
+                return instance;
+            instance = new SwitchLanguage();
+
+            string languageFromFile = GetLanguageFromFile();
+
+            //// Is there a ModuleManager cache
+            //if (File.Exists(cachePath))
+            //{
+            //    string languageFromCache = GetLanguageFromCache();
+            //    if (languageFromFile != languageFromCache)
+            //    {
+            //        Debug.Log("[SL] Language was changed by Module Manager");
+            //        languageFromFile = languageFromCache;
+            //    }
+            //}
+
+            Debug.Log("[SL] Switching language to: " + languageFromFile);
+            Localization.instance.SwitchToLanguage(languageFromFile);
+            return instance;
+        }
+
+        private static string GetLanguageFromFile()
+        {
+            string language = Localization.instance.CurrentLanguage;
+            if (File.Exists(filePath))
+            {
+                foreach (string readAllLine in File.ReadAllLines(filePath))
+                {
+                    if (readAllLine.Contains("language") && readAllLine.Contains("="))
+                    {
+                        string[] strArray = readAllLine.Split('=');
+                        if (strArray.Length == 2)
+                            language = strArray[1].Trim();
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("[SL] Could not find settings file! Please download SwitchLanguage again!");
+
+                PopupDialog.SpawnPopupDialog(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f), "SwitchLanguageMsg",
+                    "Switch Language", "Could not find settings file! Please download SwitchLanguage again!", "Ok",
+                    true, HighLogic.UISkin);
+            }
+            return language;
+        }
+
+        //private static string GetLanguageFromCache()
+        //{
+        //    string language = Localization.instance.CurrentLanguage;
+        //    foreach (ConfigNode node in ConfigNode.Load(cachePath).GetNodes("UrlConfig"))
+        //    {
+        //        if (node.GetValue("name").Contains("SwitchLanguage"))
+        //        {
+        //            if (node.HasNode("SwitchLanguage"))
+        //            {
+        //                ConfigNode settings = node.GetNode("SwitchLanguage");
+        //                language = settings.HasValue("language") ? settings.GetValue("language").Trim() : language;
+        //            }
+        //        }
+        //    }
+        //    return language;
+        //}
+    }
+}
